15d4f071ff56ca88ca73ab8a84180482141e48d9
dewirizki <dewirizkiftrn@gmail.com>
2022-10-14 22:36:41 +0700

    update code

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
 bot_token = "5722413336:AAEa-dH_C1LsKWyPrAJSYb0qA0OLCHFTuwY"
-bot_user_name = "HoPo"
-URL = "https://hoporesearch.herokuapp.com/"
+bot_user_name = "HolidayPocketBot"
+URL = "https://hopo.vercel.app"
